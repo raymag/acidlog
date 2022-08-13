e86be54ada58f6b60bbb8ec2d238bbea3d9cd214
raymag <prayk11@outlook.com>
2022-08-13 14:07:13 -0300

    feat: add new colors to theme

--- src/global/theme.ts
+++ src/global/theme.ts
@@ -1,10 +1,12 @@
 export default {
   colors: {
     primary: '#3EE878',
+    accent: '#fc0ac8',
     dark: '#343434',
-    // grayish: '#433E3Eee',
     grayish: '#222',
     primaryWatered: '#3EE878dd',
+    accentWatered: '#fc0ac8dd',
+    darkWatered: '#545454',
     white: '#fff',
   },
   fonts: {
@@ -16,9 +18,12 @@ export type themeType = {
   theme: {
     colors: {
       primary: string;
+      accent: string;
       dark: string;
       grayish: string;
       primaryWatered: string;
+      accentWatered: string;
+      darkWatered: string;
       white: string;
     };
     fonts: {
